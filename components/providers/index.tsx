1e48e9129db74b03b41b81b4ae84c25f56d10dcc
AmirZalaghi <zalaghi.dev@gmail.com>
2025-11-23 16:01:35 +0330

    ADD: Theme Toggler

---
+++ components/providers/index.tsx
@@ -0,0 +1,17 @@
+import { ReactNode } from "react";
+import { ThemeProvider } from "./theme-provider";
+
+const Providers = ({ children }: { children: ReactNode }) => {
+  return (
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >
+      {children}
+    </ThemeProvider>
+  );
+};
+
+export default Providers;
